a66dcb1f512c0e479f4f7883185ed1396bd6b8f7
Bugra Tosun <bugratosun112@gmail.com>
2023-01-17 18:25:50 +0300

    taksi

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args){
+        double km = 2.20 ,tl=10, mesafe, min = 20;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Gidiceginiz mesafeyi km olarak yazınız : ");
+        mesafe = inp.nextDouble();
+        
+        tl = mesafe * km;
+        System.out.print(tl <= 20 ? "Ödemeniz gereken tutar : " + min : "Ödemeniz gereken tutar :" + tl);
+
+       
+         
+    }
+}
